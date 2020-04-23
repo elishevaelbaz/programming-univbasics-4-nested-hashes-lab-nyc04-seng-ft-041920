77eae7e3d25f302988ecdf12e007fff520b6d0ed
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-23 15:16:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,8 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -130,6 +132,5 @@ def adding_to_dennis
       }
     }
   
-  programmer_hash[:alan_kay][:known_for] = "GUI"
-  programmer_hash
+  
 end
